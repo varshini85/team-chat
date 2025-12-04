5448aa0aef718bcc7ce8deaaf615b29b5b9dbaad
varshini85 <varshinisarav@gmail.com>
2025-12-04 21:50:23 +0530

    modify cors

--- team-chat-backend/app/main.py
+++ team-chat-backend/app/main.py
@@ -16,8 +16,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                    "http://127.0.0.1:5173"],  
+    allow_origins=["https://team-chat-git-main-varshini-saravanans-projects-d588e173.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
