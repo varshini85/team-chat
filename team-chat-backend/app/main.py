b4fc1e3c060c9f05e7ee4818dbb434691f295f9e
varshini85 <varshinisarav@gmail.com>
2025-12-05 10:24:01 +0530

    changes in cors url

--- team-chat-backend/app/main.py
+++ team-chat-backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5174",         
         "http://127.0.0.1:5174",
-        "https://team-chat-ktbv.vercel.app"],  
+        "https://team-chat-w9qr.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
